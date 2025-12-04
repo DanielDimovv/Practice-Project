6895c9111d2985430bec77c5acd53992be820eaf
DanielDimovv <daniel.dimov@bianor.com>
2025-12-04 08:38:31 +0200

    Initial project setup

---
+++ src/server/db/client.ts
@@ -0,0 +1,7 @@
+import "server-only";
+import Database from "better-sqlite3";
+import { drizzle } from "drizzle-orm/better-sqlite3";
+import * as schema from "./schema";
+
+const sqlite = new Database(process.env.DB_FILE_PATH!);
+export const db = drizzle(sqlite, { schema });
---
+++ src/server/db/schema.ts
@@ -0,0 +1,8 @@
+import {
+  sqliteTable,
+  text,
+  integer,
+  index,
+  primaryKey,
+} from "drizzle-orm/sqlite-core";
+import { relations, sql } from "drizzle-orm";
